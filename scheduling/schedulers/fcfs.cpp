618abc122be31c9b2a4f962590e3287d01d0ef64
Abubakkar Siddique <asiddique201329@bscse.uiu.ac.bd>
2025-09-15 20:28:56 +0600

    fcfs implemented

--- scheduling/schedulers/fcfs.cpp
+++ scheduling/schedulers/fcfs.cpp
@@ -20,6 +20,31 @@ public:
         std::cout << "Starting FCFS scheduling...\n";
         
         // EXAMPLE IMPLEMENTATION [Demo in class]
+        //Sort by arrival time
+         std::sort(processes.begin(), processes.end(),
+                  [](const Process &a, const Process &b) {
+                      return a.arrivalTime < b.arrivalTime;
+                  });
+
+        int currentTime = 0;
+
+        for (auto &p : processes) {
+            if (currentTime < p.arrivalTime)
+                currentTime = p.arrivalTime; // wait until process arrives
+
+            p.startTime = currentTime;
+            p.finishTime = currentTime + p.burstTime;
+            p.turnaroundTime = p.finishTime - p.arrivalTime;
+            p.waitingTime = p.startTime - p.arrivalTime;
+
+            currentTime += p.burstTime;
+
+            std::cout << "Process " << p.pid
+                      << " | Start: " << p.startTime
+                      << " | Finish: " << p.finishTime
+                      << " | Waiting: " << p.waitingTime
+                      << " | Turnaround: " << p.turnaroundTime << "\n";
+        }
         
         std::cout << "FCFS scheduling completed.\n";
     }
